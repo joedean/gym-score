9d34bd199d857f1d285b2134f2580f2e5f061590
Joseph Dean <joe@on-site.com>
2015-02-14 07:34:42 -0800

    Add a test for the athlete.full_name

--- spec/models/athlete_spec.rb
+++ spec/models/athlete_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Athlete, :type => :model do
-  it { is_expected.to respond_to :age }
   it { is_expected.to respond_to :full_name }
+  it { is_expected.to respond_to :age }
+
+  describe "#full_name" do
+    subject(:athlete) { build :athlete, first_name: first_name, last_name: last_name }
+
+    context "when athlete has a first and last name" do
+      let(:first_name) { "John" }
+      let(:last_name) { "Dean" }
+
+      it "generates the correct full name" do
+        expect(athlete.full_name).to eql("John Dean")
+      end
+    end
+
+  end
 
   describe "#age" do
     subject(:athlete) { build :athlete, birth_date: birth_date }
