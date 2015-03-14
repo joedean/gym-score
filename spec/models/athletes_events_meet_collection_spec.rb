870b3448ee24351c262d65bf9ee793fa533e13a1
Joseph Dean <joe@on-site.com>
2015-03-14 12:34:30 -0700

    Add test for calculating overall_score

--- spec/models/athletes_events_meet_collection_spec.rb
+++ spec/models/athletes_events_meet_collection_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 RSpec.describe AthletesEventsMeetCollection, type: :model do
   describe "#overall_score" do
-    let!(:athletes_events_meets) { create :athletes_events_meet }
+    let(:athletes_events_meets) { build_list :athletes_events_meet, 3,
+                                         score: 9.0 }
+    let(:collection) { AthletesEventsMeetCollection.new({}) }
+
+    before do
+      collection.stub(:athletes_events_meets).and_return(athletes_events_meets)
+    end
 
     it "sums all event scores for the meet" do
-      binding.pry
-
+      expect(collection.overall_score).to eq(27.0)
     end
   end
 end
